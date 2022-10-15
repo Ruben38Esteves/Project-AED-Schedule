dd6070c17a2d238e7b2cea66daae00763899b67b
Ruben38Esteves <ruben38esteves@gmailcom>
2022-10-15 15:46:24 +0100

    Ler o ficheiro classes_per_uc e colocar num vetor

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "read.h"
+
+int main() {
+    Read teste1;
+    teste1.read_classes_per_uc();
+    return 0;
+}
--- src/read.cpp
+++ src/read.cpp
@@ -1,1 +1,35 @@
-ola um dois tres teste
+#include "read.h"
+
+vector<classes_per_uc> Read::read_classes_per_uc(){
+    vector<classes_per_uc> cpu;
+    //abrir ficheiro
+    ifstream file_cpu;
+    file_cpu.open("../classes_per_uc.csv");
+    //verificar se foi possivel abrir
+    if(!file_cpu.is_open()){
+        return cpu;
+    }
+
+    string code;
+    //ir buscar primeira linha (UcCode,ClassCode)
+    getline(file_cpu, code);
+    //ler linha a linha
+    while(getline(file_cpu, code)){
+        stringstream line(code);
+        string buffer;
+        classes_per_uc temp;
+        //buscar UcCode (le ate ',')
+        getline(line, buffer, ',');
+        temp.UcCode = buffer;
+        //buscar ClassCode (le ate fim de linha)
+        getline(line, buffer, '\r');
+        temp.ClassCode = buffer;
+        //adicionar a vetor
+        cpu.push_back(temp);
+    }
+    //teste
+    for(auto teste : cpu){
+        cout << "UC:" << teste.UcCode << "   Class:" << teste.ClassCode << '\n';
+    }
+    return cpu;
+}
