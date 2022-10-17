2302e38af67f9866b9110a7eff905c8cd5f02634
Ruben38Esteves <ruben38esteves@gmailcom>
2022-10-17 18:39:25 +0100

    bbbbb

--- src/main.cpp
+++ src/main.cpp
@@ -163,11 +163,8 @@ int main() {
                         string StudentNameOrCode;
                         cout << "Escolha o aluno" << '\n';
                         cin >> StudentNameOrCode;
-                        
                         Horario_Aluno escolhido(StudentNameOrCode,clss);
-                        
                         escolhido.Print_Horario_Sorted();
-                        
                         break;
                     }
                 }
